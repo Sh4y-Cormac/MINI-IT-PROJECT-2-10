718d614b5c072bd4398c5fa593855fcc6ac45d82
Luqman Hakim Bin Muhammad Fahmi <luqman5421er@gmail.com>
2025-04-08 17:11:56 +0800

    Start Screen
    Sound Effects Added / Cuztomize Button Added / Did Some Coding

---
+++ GUI Stuff/start_menu.gd
@@ -0,0 +1,21 @@
+extends Control
+
+
+
+func _on_button_2_pressed() -> void:
+	$"Clicking And Hovering".play()
+
+func _on_button_2_mouse_entered() -> void:
+	$"Clicking And Hovering".play()
+
+func _on_button_pressed() -> void:
+	$"Clicking And Hovering".play()
+
+func _on_button_mouse_entered() -> void:
+	$"Clicking And Hovering".play()
+
+func _on_button_3_pressed() -> void:
+	$"Clicking And Hovering".play()
+
+func _on_button_3_mouse_entered() -> void:
+	$"Clicking And Hovering".play()
