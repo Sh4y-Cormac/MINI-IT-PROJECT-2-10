a2e9de9d0f41f42c095bf5741b7758bafed0da87
Luqman Hakim Bin Muhammad Fahmi <luqman5421er@gmail.com>
2025-04-12 22:12:43 +0800

    Start Menu
    Function Feedback Button ( Not Complete ) / Animations Added

--- GUI Stuff/start_menu.gd
+++ GUI Stuff/start_menu.gd
@@ -1,27 +1,26 @@
 extends Control
 
+@export var Options : NinePatchRect
+
+@export var animation_player : AnimationPlayer
+
+func hide_and_show(first : String):
+	animation_player.play("show_" + first)
+
+enum STATE { FEEDBACK }
 
 
-func _on_button_2_pressed() -> void:
+func _on_feedback_button_pressed() -> void:
+	STATE.FEEDBACK
+	$"Clicking And Hovering".play()
+	hide_and_show("feedback")
+
+
+func _on_start_button_mouse_entered() -> void:
 	$"Clicking And Hovering".play()
 
-func _on_button_2_mouse_entered() -> void:
+func _on_customise_button_mouse_entered() -> void:
 	$"Clicking And Hovering".play()
 
-func _on_button_pressed() -> void:
-	$"Clicking And Hovering".play()
-
-func _on_button_mouse_entered() -> void:
-	$"Clicking And Hovering".play()
-
-func _on_button_3_pressed() -> void:
-	$"Clicking And Hovering".play()
-
-func _on_button_3_mouse_entered() -> void:
-	$"Clicking And Hovering".play()
-
-func _on_button_4_pressed() -> void:
-	$"Clicking And Hovering".play()
-
-func _on_button_4_mouse_entered() -> void:
+func _on_load_button_mouse_entered() -> void:
 	$"Clicking And Hovering".play()
