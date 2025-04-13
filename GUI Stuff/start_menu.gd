5c9ee4fbe1fd550cd62280edb5814611e0b1868b
Umar <umarzaidtenby@gmail.com>
2025-04-13 21:32:19 +0800

    added the map
    tileset is active, plus a basic character

--- GUI Stuff/start_menu.gd
+++ GUI Stuff/start_menu.gd
@@ -24,3 +24,7 @@ func _on_customise_button_mouse_entered() -> void:
 
 func _on_load_button_mouse_entered() -> void:
 	$"Clicking And Hovering".play()
+
+
+func _on_start_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/game.tscn")
