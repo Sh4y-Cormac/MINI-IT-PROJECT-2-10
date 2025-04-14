b98446f4dadf085ba621320b857e59d06058439f
Luqman Hakim Bin Muhammad Fahmi <luqman5421er@gmail.com>
2025-04-14 20:07:41 +0800

    Game Intro
    Fade In / Fade Out

--- GUI Stuff/intro.gd
+++ GUI Stuff/intro.gd
@@ -2,9 +2,9 @@ extends Node2D
 
 func _ready():
 	$AnimationPlayer.play("Fade in")
-	await get_tree().create_timer(6).timeout
+	await get_tree().create_timer(4).timeout
 	$AnimationPlayer.play("Typewriter")
-	await $AnimationPlayer.animation_finished
+	await get_tree().create_timer(6).timeout
 	$AnimationPlayer.play("Fade out")
 	await get_tree().create_timer(3).timeout
 	get_tree().change_scene_to_file("res://GUI Stuff/StartMenu.tscn")
