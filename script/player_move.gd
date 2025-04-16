76cdb1a8992cf8901d6aa5eb172d2ad0f59a1fed
Aisyah <aisyahahmadnizam@gmail.com>
2025-04-16 23:25:46 +0800

    added dash

--- script/player_move.gd
+++ script/player_move.gd
@@ -5,6 +5,10 @@ const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
 var jump_count = 0
 
+const DASH_SPEED = 500.0
+var dashing = false 
+var can_dash = true
+
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 @onready var animated_sprites = $AnimatedSprite2D
@@ -20,9 +24,22 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("jump") and jump_count < 2:
 		jump_count += 1
 		velocity.y = JUMP_VELOCITY
+		
+	if Input.is_action_just_pressed("dash") and can_dash:
+		dashing = true
+		can_dash = false
+		$dash_timer.start()
+		$dash_again_timer.start()
 
 	# Get the input direction : -1, 0, 1
 	var direction := Input.get_axis("move_left", "move_right")
+	if direction:
+		if dashing:
+			velocity.x = direction * DASH_SPEED
+		else:
+			velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
 	
 	# flip sprite
 	if direction > 0:
@@ -39,10 +56,12 @@ func _physics_process(delta: float) -> void:
 	else:
 		animated_sprites.play("jump")
 		
-	# apply movement
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+#make it stop dashing
+func _on_dashtimer_timeout() -> void:
+	dashing = false
+
+func _on_dash_again_timer_timeout() -> void:
+	can_dash = true
