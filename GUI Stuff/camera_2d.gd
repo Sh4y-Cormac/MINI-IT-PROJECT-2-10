0c342641c9605af90c6aecdf83740b64ad628b42
Luqman Hakim Bin Muhammad Fahmi <luqman5421er@gmail.com>
2025-04-18 06:08:19 +0800

    camera tweaks

--- GUI Stuff/camera_2d.gd
+++ GUI Stuff/camera_2d.gd
@@ -1,11 +1,11 @@
 extends Camera2D
 
-const Dead_Zone = 100
+const Dead_Zone = 300
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
 		var _target = event.position - get_viewport().size * 0.5
 		if _target.length() < Dead_Zone:
-			self.position = Vector2(0,0)
+			self.position = Vector2(246,38)
 		else:
 			self.position = _target.normalized() * (_target.length() - Dead_Zone) * 0.5 
