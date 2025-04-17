b1832508fc1955d35eaa58e869d2d0eab5320c36
Luqman Hakim Bin Muhammad Fahmi <luqman5421er@gmail.com>
2025-04-17 16:14:14 +0800

    Start Menu
    Feedback Button operational

--- GUI Stuff/start_menu.gd
+++ GUI Stuff/start_menu.gd
@@ -14,11 +14,7 @@ func show_animation(first : String):
 func hide_animation(first : String):
 	animation_player.play("hide_" + first)
 
-
 enum STATE { FEEDBACK }
-
-
-
 
 func _on_feedback_button_pressed() -> void:
 	STATE.FEEDBACK
@@ -98,6 +94,41 @@ func _on_exit_button_2_mouse_entered() -> void:
 func _on_exit_button_2_pressed() -> void:
 	$"Enter Sound Effect".play()
 	hide_animation("customize")
+	$"VBoxContainer/Start Button".disabled = false
+	$"VBoxContainer/Customise Button".disabled = false
+	$"VBoxContainer/Load Button".disabled = false
+	$"Feedback Button".disabled = false
+
+func _on_real_feedback_mouse_entered() -> void:
+	$"Clicking And Hovering".play()
+
+func _on_real_feedback_pressed() -> void:
+	$"Enter Sound Effect".play()
+	show_animation("option")
+	$"VBoxContainer/Start Button".disabled = true
+	$"VBoxContainer/Customise Button".disabled = true
+	$"VBoxContainer/Load Button".disabled = true
+	$"Feedback Button".disabled = true
+	
+	
+func _on_exit_button_3_mouse_entered() -> void:
+	$"Clicking And Hovering".play()
+	
+func _on_exit_button_3_pressed() -> void:
+	$"Enter Sound Effect".play()
+	hide_animation("option")
+	$"VBoxContainer/Start Button".disabled = false
+	$"VBoxContainer/Customise Button".disabled = false
+	$"VBoxContainer/Load Button".disabled = false
+	$"Feedback Button".disabled = false
+	
+
+func _on_save_2_mouse_entered() -> void:
+	$"Clicking And Hovering".play()
+	
+func _on_save_2_pressed() -> void:
+	$"Enter Sound Effect".play()
+	hide_animation("option")
 	$"VBoxContainer/Start Button".disabled = false
 	$"VBoxContainer/Customise Button".disabled = false
 	$"VBoxContainer/Load Button".disabled = false
