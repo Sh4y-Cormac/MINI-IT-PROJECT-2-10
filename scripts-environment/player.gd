00327729fc0d99224283939fdb6ce66517eda181
Umar <umarzaidtenby@gmail.com>
2025-05-03 17:14:06 +0800

    added acceleration and deceleration to the character

--- scripts-environment/player.gd
+++ scripts-environment/player.gd
@@ -1,9 +1,11 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+@export var walk_speed = 300.0
+@export_range(0,1) var acceleration = 0.2
+@export_range(0,1) var deceleration = 0.2
+
 const JUMP_VELOCITY = -500.0
-const gravity = 1000
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -11,15 +13,15 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("left", "right")
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = move_toward(velocity.x, direction * walk_speed, walk_speed * acceleration)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, walk_speed * deceleration)
 
 	move_and_slide()
