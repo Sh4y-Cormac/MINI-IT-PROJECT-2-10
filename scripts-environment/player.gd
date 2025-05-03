466b5bfaf224961ac6e6a77ba00452606c8c03af
Umar <umarzaidtenby@gmail.com>
2025-05-03 18:10:50 +0800

    added idle anim and run anim

--- scripts-environment/player.gd
+++ scripts-environment/player.gd
@@ -53,6 +53,12 @@ func _physics_process(delta: float) -> void:
 	elif direction < 0:
 		animated_sprite.flip_h = true
 		
+	# Play animation
+	if direction == 0:
+		animated_sprite.play("idle")
+	else:
+		animated_sprite.play("run")
+		
 	#dash mechanic
 	if Input.is_action_just_pressed("dash") and direction and not is_dashing and dash_timer <= 0:
 		is_dashing = true
