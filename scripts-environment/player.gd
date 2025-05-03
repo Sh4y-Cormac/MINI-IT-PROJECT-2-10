a9646723677346cd7965a04532f919aa84feb435
Umar <umarzaidtenby@gmail.com>
2025-05-03 17:24:10 +0800

    added running, and variable jump

--- scripts-environment/player.gd
+++ scripts-environment/player.gd
@@ -1,11 +1,13 @@
 extends CharacterBody2D
 
 
-@export var walk_speed = 300.0
+@export var walk_speed = 200.0
+@export var run_speed = 400.0
 @export_range(0,1) var acceleration = 0.2
 @export_range(0,1) var deceleration = 0.2
 
-const JUMP_VELOCITY = -500.0
+@export var jump_force = -500.0
+@export_range(0,1) var decelerate_on_jump_release = 0.5
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -14,13 +16,22 @@ func _physics_process(delta: float) -> void:
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		velocity.y = jump_force
+	
+	if Input.is_action_just_released("jump") and velocity.y < 0:
+		velocity.y *= decelerate_on_jump_release
+	
+	var speed
+	if Input.is_action_pressed("run"):
+		speed = run_speed
+	else:
+		speed = walk_speed
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("left", "right")
 	if direction:
-		velocity.x = move_toward(velocity.x, direction * walk_speed, walk_speed * acceleration)
+		velocity.x = move_toward(velocity.x, direction * speed, speed * acceleration)
 	else:
 		velocity.x = move_toward(velocity.x, 0, walk_speed * deceleration)
 
