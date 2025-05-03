0bc0d0c4925fb0fd4bd8ef3132a1c00cfc7585a4
Umar <umarzaidtenby@gmail.com>
2025-05-03 17:30:00 +0800

    added wall_jump and camera smoothing

--- scripts-environment/player.gd
+++ scripts-environment/player.gd
@@ -15,7 +15,7 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and (is_on_floor() or is_on_wall()):
 		velocity.y = jump_force
 	
 	if Input.is_action_just_released("jump") and velocity.y < 0:
