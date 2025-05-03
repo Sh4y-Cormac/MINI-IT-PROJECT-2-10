4e2397c39e25bd2257fef256e8e222ff2c5615f0
Umar <umarzaidtenby@gmail.com>
2025-05-03 17:48:26 +0800

    added dashing mechanic by pressing 'Q'

--- scripts-environment/player.gd
+++ scripts-environment/player.gd
@@ -9,6 +9,17 @@ extends CharacterBody2D
 @export var jump_force = -500.0
 @export_range(0,1) var decelerate_on_jump_release = 0.5
 
+@export var dash_speed = 1000
+@export var dash_max_distance = 150.0
+@export var dash_curve : Curve
+@export var dash_cooldown = 1.0
+
+var is_dashing = false
+var dash_start_position = 0
+var dash_direction = 0
+var dash_timer = 0
+
+
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
@@ -34,5 +45,25 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, direction * speed, speed * acceleration)
 	else:
 		velocity.x = move_toward(velocity.x, 0, walk_speed * deceleration)
-
+	
+	#dash mechanic
+	if Input.is_action_just_pressed("dash") and direction and not is_dashing and dash_timer <= 0:
+		is_dashing = true
+		dash_start_position = position.x
+		dash_direction = direction
+		dash_timer = dash_cooldown
+	
+	#performs actual dash
+	if is_dashing:
+		var current_distance = abs(position.x - dash_start_position)
+		if current_distance >= dash_max_distance or is_on_wall():
+			is_dashing = false
+		else:
+			velocity.x = dash_direction * dash_speed * dash_curve.sample(current_distance/dash_max_distance)
+			velocity.y = 0
+	
+	#reduces dash timer
+	if dash_timer > 0:
+		dash_timer -= delta
+	
 	move_and_slide()
