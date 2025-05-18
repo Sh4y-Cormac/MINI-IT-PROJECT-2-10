1ab137419103f20a7b729a7d937702c461232236
Aisyah <aisyahahmadnizam@gmail.com>
2025-05-18 20:33:09 +0800

    environment desert

--- script-obstacles/tumbleweed.gd
+++ script-obstacles/tumbleweed.gd
@@ -1,35 +1,38 @@
 extends CharacterBody2D
 
 @export var speed: float = 100.0
-@export var max_distance: float = 150.0
-@onready var tumbleweedtimer = $"../tumbleweedkill/Timer"
+@export var max_distance: float = 200.0
 
 var direction := 1
 var start_position := Vector2.ZERO
+var can_move := true
+var is_blocked_by_player := false
 
 func _ready():
 	start_position = global_position 
 
 func _physics_process(delta):
-	velocity.x = speed * direction
-	move_and_slide()
+	if can_move:
+		var motion = Vector2(speed * direction, 0) * delta
+		var collision = move_and_collide(motion)
+
+		if collision:
+			if collision.get_collider().name == "player":
+				can_move = false
+				is_blocked_by_player = true
+			else:
+				direction *= -1
+
+	else:
+		var test_motion = Vector2(speed * direction, 0) * delta
+		var test_collision = move_and_collide(test_motion, true)
+		
+		if test_collision == null or test_collision.get_collider().name != "player":
+			can_move = true
+			is_blocked_by_player = false
 
 	if abs(global_position.x - start_position.x) >= max_distance:
 		direction *= -1
 
 	if $Sprite2D:
 		$Sprite2D.rotation += direction * speed * delta / 50.0
-		
-
-func _on_tumbleweedkill_body_entered(body: Node2D) -> void:
-	if body.name == "player":
-		var col = body.get_node_or_null("CollisionShape2D")
-		if col:
-			col.queue_free()
-		print("Player hit the killzone")
-		tumbleweedtimer.start()
-
-
-func _on_timer_timeout() -> void:
-	Engine.time_scale = 1.0
-	get_tree().reload_current_scene()
