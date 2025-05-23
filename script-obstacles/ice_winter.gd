16e9d45c6db3f09a3b7dc23c4cccefbb93518adc
Aisyah <aisyahahmadnizam@gmail.com>
2025-05-23 09:00:31 +0800

    environment winter

---
+++ script-obstacles/ice_winter.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+@onready var icetimer = $Sprite2D/ice_kill/ice_timer
+
+
+func _on_ice_kill_body_entered(body: Node2D) -> void:
+	pass # Replace with function body.
+
+
+func _on_ice_timer_timeout() -> void:
+	pass # Replace with function body.
