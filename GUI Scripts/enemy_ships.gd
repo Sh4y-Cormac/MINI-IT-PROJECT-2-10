d0cb768ef22f51b3880420f4a4a433a47dbed835
Luqman Hakim Bin Muhammad Fahmi <luqman5421er@gmail.com>
2025-05-25 02:49:31 +0800

    Laser Destroy Enemy / Collision with enemy destroy player

---
+++ GUI Scripts/enemy_ships.gd
@@ -0,0 +1,18 @@
+class_name Enemy extends Area2D
+
+@export var speed = 150
+
+func _physics_process(delta):
+	global_position.y += speed * delta
+
+func destroyed():
+	queue_free()
+
+
+func _on_body_entered(body):
+	if body is SpaceShip:
+		body.destroyed()
+		destroyed()
+
+func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+	queue_free()
