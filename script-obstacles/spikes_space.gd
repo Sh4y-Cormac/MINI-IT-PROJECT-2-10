e6d954f80abfdce43477a9b17f234f4eeef025fa
Aisyah <aisyahahmadnizam@gmail.com>
2025-06-01 15:37:49 +0800

    environment space

--- script-obstacles/spikes_space.gd
+++ script-obstacles/spikes_space.gd
@@ -4,3 +4,7 @@ extends Node2D
 func _on_spike_killzone_body_entered(body: Node2D) -> void:
 	if body.name == "player":
 		get_tree().reload_current_scene()
+
+
+func _on_timer_timeout() -> void:
+	pass # Replace with function body.
