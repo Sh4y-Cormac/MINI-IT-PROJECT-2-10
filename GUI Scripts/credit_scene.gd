f4d6ff352d4066084a649945f44eb6b9e52459df
Luqman Hakim Bin Muhammad Fahmi <luqman5421er@gmail.com>
2025-06-01 18:00:20 +0800

    Credit Scene

---
+++ GUI Scripts/credit_scene.gd
@@ -0,0 +1,6 @@
+extends Control
+
+func _ready():
+	$AnimationPlayer.play("Credit_Scene")
+	await $AnimationPlayer.animation_finished
+	Functions.load_screen_to_scene("res://GUI Scenes/StartMenu.tscn")
