f8505ed38a75dfd481ea211e51b6568c3044f3d0
Aisyah <aisyahahmadnizam@gmail.com>
2025-06-02 01:14:57 +0800

    environment space

--- script-obstacles/saw_space.gd
+++ script-obstacles/saw_space.gd
@@ -1,10 +1,14 @@
 extends Node2D
 
 @onready var saw_timer = $saw_killzone/saw_timer
+@onready var collision_shape = $saw_killzone/CollisionShape2D
+var player_reference: Node2D = null
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.name == "player":
-		$saw_killzone/CollisionShape2D.disabled = true
+		var col = body.get_node_or_null("CollisionShape2D")
+		if col:
+			col.queue_free()
 		saw_timer.start()
 
 func _on_timer_timeout() -> void:
--- script-obstacles/spikes_space.gd
+++ script-obstacles/spikes_space.gd
@@ -1,10 +1,17 @@
 extends Node2D
 
+@onready var spike_timer = $spike_killzone/Timer
+@onready var collision_shape = $spike_killzone/CollisionShape2D
+var player_reference: Node2D = null
+
 
 func _on_spike_killzone_body_entered(body: Node2D) -> void:
 	if body.name == "player":
-		get_tree().reload_current_scene()
-
+		var col = body.get_node_or_null("CollisionShape2D")
+		if col:
+			col.queue_free()
+		spike_timer.start()
 
 func _on_timer_timeout() -> void:
-	pass # Replace with function body.
+	Engine.time_scale = 1.0
+	get_tree().reload_current_scene()
