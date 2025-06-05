709eb65b99adba9eb86bc593ce09320ea7703e64
Aisyah <aisyahahmadnizam@gmail.com>
2025-06-05 11:54:28 +0800

    cutscene

--- cutscene/cutscene_1_news.gd
+++ cutscene/cutscene_1_news.gd
@@ -10,37 +10,37 @@ var typing_speed := 0.03
 var dialogue_count = 0
 
 func _ready():
-	start_dialogue("Breaking news: Chaos erupts worldwide as an
-	 unknown force invades Earth. ")
+	start_dialogue("Breaking news: Chaos erupts worldwide as an\nunknown force invades Earth.")
 	dialogue_count = 1
 
 func _process(delta):
-	if Input.is_action_just_pressed("ui_accept"): 
+	if Input.is_action_just_pressed("ui_accept"):
 		if is_typing:
 			skip_typing()
 		else:
 			if dialogue_count == 1:
-				start_dialogue("The Zephyrus has landed with one goal —
-				 total control over human evolution.")
+				start_dialogue("The Zephyrus has landed with one goal — 
+				total control over human evolution.")
 				dialogue_count += 1
 			elif dialogue_count == 2:
-				start_dialogue("Instead of peace, they’ve chosen destruction.
-				 Cities are crumbling. Panic is everywhere.")
+				start_dialogue("Instead of peace, they’ve chosen destruction. 
+				Cities are crumbling. Panic is everywhere.")
 				dialogue_count += 1
 			elif dialogue_count == 3:
 				start_dialogue("Worse, they’ve released a fast-spreading virus, 
 				infecting our own fallen soldiers.")
 				dialogue_count += 1
-			elif dialogue_count ==4:
-				start_dialogue("Most of our defenses are down...
-				 only one soldier remains.")
+			elif dialogue_count == 4:
+				start_dialogue("Most of our defenses are down... 
+				only one soldier remains.")
 				dialogue_count += 1
 			elif dialogue_count == 5:
-				start_dialogue("Oh—hold on. We’re getting a transmission from 
-			one of our experts. A message for the last hope.")
-				
+				start_dialogue("Oh—hold on. We’re getting a transmission from one of our experts. 
+				A message for the last hope.")
+				dialogue_count += 1
 			else:
-				print("no more")
+				print("No more dialogue.")
+
 
 func start_dialogue(text: String):
 	full_text = text
@@ -63,4 +63,5 @@ func type_next_letter():
 
 func skip_typing():
 	is_typing = false
+	current_text = full_text
 	dialogue_label.text = full_text
