db302ded8e9e9bf3002cb0ba64fe5dacd555b366
Luqman Hakim Bin Muhammad Fahmi <luqman5421er@gmail.com>
2025-06-05 14:30:59 +0800

    Minigame Close Functions And Resizable not possible

--- GUI Scripts/space_ship_game_over.gd
+++ GUI Scripts/space_ship_game_over.gd
@@ -1,12 +1,5 @@
 extends Control
 
-
-func _on_restart_pressed() -> void:
-	$"Sound Effects/Enter Sound Effect".play()
-	await $"Sound Effects/Enter Sound Effect".finished
-	get_tree().paused = false
-	get_tree().reload_current_scene()
-
 func _on_restart_mouse_entered() -> void:
 	$"Sound Effects/Hovering Sound Effect".play()
 
@@ -14,17 +7,5 @@ func set_score(value):
 	$Panel/GoldEarnedLabel.text = "Gold Earned: " + str(value)
 
 
-func _on_quit_pressed() -> void:
-	$"Sound Effects/Enter Sound Effect".play()
-	await $"Sound Effects/Enter Sound Effect".finished
-	close()
-
-
 func _on_quit_mouse_entered() -> void:
 	$"Sound Effects/Hovering Sound Effect".play()
-
-func close():
-	get_tree().paused = false
-	var exist = get_node_or_null("SpaceShooterWindow")
-	if exist:
-		exist.queue_free()
--- GUI Scripts/spaceshipwindow.gd
+++ GUI Scripts/spaceshipwindow.gd
@@ -9,3 +9,21 @@ func _process(delta):
 	background.scroll_offset.y += delta * scroll_speed
 	if background.scroll_offset.y > 500:
 		background.scroll_offset.y = 0
+
+
+func _on_quit_pressed() -> void:
+	$"SpaceShooter/UI layer/SpaceShipGameOver/Sound Effects/Enter Sound Effect".play()
+	await $"SpaceShooter/UI layer/SpaceShipGameOver/Sound Effects/Enter Sound Effect".finished
+	get_tree().paused = false
+	queue_free()
+
+
+func _on_restart_pressed() -> void:
+	$"SpaceShooter/UI layer/SpaceShipGameOver/Sound Effects/Enter Sound Effect".play()
+	await $"SpaceShooter/UI layer/SpaceShipGameOver/Sound Effects/Enter Sound Effect".finished
+
+
+
+func _on_close_requested() -> void:
+	get_tree().paused = false
+	queue_free()
