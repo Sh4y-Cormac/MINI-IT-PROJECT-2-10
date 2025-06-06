6b1da037eb498a01d366608af17bc615816a90ca
Luqman Hakim Bin Muhammad Fahmi <luqman5421er@gmail.com>
2025-06-06 23:42:03 +0800

    Game over scene

---
+++ GUI Scripts/gameover.gd
@@ -0,0 +1,33 @@
+extends Control
+
+func _ready():
+	$AnimationPlayer.play("Fade In")
+
+func _on_restart_button_mouse_entered() -> void:
+	$"audio/Hovering Sound Effect".play()
+	
+func _on_return_to_menu_mouse_entered() -> void:
+	$"audio/Hovering Sound Effect".play()
+
+
+func _on_quit_button_mouse_entered() -> void:
+	$"audio/Hovering Sound Effect".play()
+
+
+
+func _on_restart_button_pressed() -> void:
+	$"audio/Enter Sound Effect".play()
+	await $"audio/Enter Sound Effect".finished
+	Functions.load_screen_to_scene("res://scenes-environment/game.tscn")
+
+
+func _on_return_to_menu_pressed() -> void:
+	$"audio/Enter Sound Effect".play()
+	await $"audio/Enter Sound Effect".finished
+	Functions.load_screen_to_scene("res://GUI Scenes/StartMenu.tscn")
+
+
+func _on_quit_button_pressed() -> void:
+	$"audio/Enter Sound Effect".play()
+	await $"audio/Enter Sound Effect".finished
+	get_tree().quit()
