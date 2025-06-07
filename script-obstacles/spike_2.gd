8c346a74b17c8e5121d7261aab4766b89a6d4b09
Aisyah <aisyahahmadnizam@gmail.com>
2025-06-07 21:12:42 +0800

    debugging
    player and platforms had collision problem, all fixed

--- script-obstacles/spike_2.gd
+++ script-obstacles/spike_2.gd
@@ -4,13 +4,14 @@ extends Node2D
 @onready var collision_shape = $spike2/CollisionShape2D
 var player_reference: Node2D = null
 
-func _on_spike_1_body_shape_entered(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
+func _on_spike_2_body_shape_entered(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
+	
 	if body.name == "player":
 		var col = body.get_node_or_null("CollisionShape2D")
 		if col:
 			col.queue_free()
 		spike2_timer.start()
-
+		
 func _on_timer_timeout() -> void:
 	Engine.time_scale = 1.0
 	get_tree().reload_current_scene()
