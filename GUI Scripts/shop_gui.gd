8518af08243a096d78c661107ec39ee45d01483f
Luqman Hakim Bin Muhammad Fahmi <luqman5421er@gmail.com>
2025-06-07 21:52:08 +0800

    Shop (Purchase depends on the cost)

--- GUI Scripts/shop_gui.gd
+++ GUI Scripts/shop_gui.gd
@@ -1,29 +1,33 @@
 extends Control
 
 @export var description : NinePatchRect
+var selected_item : Item
 
-var gold : int = 10000
+var gold : int = 5
+
+func _ready():
+	$"NinePatchRect/Coins Display/Label".text = "Gold: " + str(gold)
 
 
 func set_description(item : Item):
+	selected_item = item
 	description.find_child("Item Name").text = item.title
 	description.find_child("Icon").texture = item.icon
 	description.find_child("Description").text = item.description  
-	description.find_child("Cost").text = item.price
+	description.find_child("Cost").text = "$" + str(item.price)
 	
 
 	
 func update_gold_label():
-	$"Coins Display/Label".text = "Gold: " + str(gold)
+	$"NinePatchRect/Coins Display/Label".text = "Gold: " + str(gold)
 
 func _on_buy_pressed() -> void:
-		var price = 100  
-		if gold >= price:
-			gold -= price
-			print("Bought item! New gold: ", gold)
-			update_gold_label()
-		else:
-			print("Not enough gold!")
+	if selected_item and gold >= selected_item.price:
+		gold -= selected_item.price
+		update_gold_label()
+		print("Bought:", selected_item.title)
+	else:
+		print("Not enough gold!")
 
 
 func _on_button_mouse_entered() -> void:
