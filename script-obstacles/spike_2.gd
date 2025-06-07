6104c64c1d5cd76c7b9e7428360fecd51fc423d0
Aisyah <aisyahahmadnizam@gmail.com>
2025-06-07 20:58:28 +0800

    spikes killzone

--- script-obstacles/spike_2.gd
+++ script-obstacles/spike_2.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
-@onready var spike1_timer = $spike1/Timer
-@onready var collision_shape = $spike1/CollisionShape2D
+@onready var spike2_timer = $spike2/Timer
+@onready var collision_shape = $spike2/CollisionShape2D
 var player_reference: Node2D = null
 
 func _on_spike_1_body_shape_entered(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
@@ -9,7 +9,7 @@ func _on_spike_1_body_shape_entered(body_rid: RID, body: Node2D, body_shape_inde
 		var col = body.get_node_or_null("CollisionShape2D")
 		if col:
 			col.queue_free()
-		spike1_timer.start()
+		spike2_timer.start()
 
 func _on_timer_timeout() -> void:
 	Engine.time_scale = 1.0
