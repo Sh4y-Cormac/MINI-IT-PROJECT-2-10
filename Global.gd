428a6a00c4031e2b984a99aeaeb9fcee4edab7e8
Aisyah <aisyahahmadnizam@gmail.com>
2025-06-07 21:59:55 +0800

    health heart

--- Global.gd
+++ Global.gd
@@ -13,7 +13,6 @@ var playerHitbox: Area2D
 var playerGold: int #amount of gold player has
 var playerAlive: bool
 var playerSkin: int #what skin the player has. Will determine the animations and stuff
-var playerHealth: int #the health of the player
 
 ## Entity Values
 var robotDamageZone: Area2D
@@ -21,3 +20,27 @@ var robotDamageAmount: int
 
 var golemDamageZone: Area2D
 var golemDamageAmount: int
+
+## Player Health
+
+var playerHealth: int = 100
+var health: int = 5
+var max_health: int = 5
+var hearts_list: Array[TextureRect] = []
+
+func setup_hearts(hearts_parent):
+	hearts_list.clear()
+	for child in hearts_parent.get_children():
+		hearts_list.append(child)
+	update_heart_display()
+	
+func take_damage():
+	if health > 0:
+		health -= 1
+		update_heart_display()
+		if health == 0:
+			print("Player dead")
+			
+func update_heart_display():
+	for i in range(hearts_list.size()):
+		hearts_list[i].visible = i < health
