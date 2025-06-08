7b1c5c0ccfcb823ca15eff17fe9d69948c6c7571
Luqman Hakim Bin Muhammad Fahmi <luqman5421er@gmail.com>
2025-06-08 23:54:50 +0800

    Inventory Redo

--- GUI Scripts/inventory_slot.gd
+++ GUI Scripts/inventory_slot.gd
@@ -1,24 +1,53 @@
-extends TextureRect
+extends Panel
 
-@export var itemResource : Item
-
-func set_new_data(resource: Item):
-	itemResource = resource
-	
-	if resource != null:
-		texture = itemResource.icon
-		itemResource.inventarSlot = get_parent().name
-		itemResource.InventarPosition = int(name.split("Slot")[1])
-	else:
-		itemResource = null
-		texture = null
+@export var item : Item = null:
+	set(value):
+		item = value
 		
-func get_slot_name():
-	var parentName = get_parent().name
-	var slotNumber = name.split("Slot")[1]
-	
-	return parentName + slotNumber
+		if value == null:
+			$Icon.texture = null
+			return
+		
+		$Icon.texture = value.icon
 
-func delete_resource():
-	texture = null
-	itemResource = null
+@export var amount : int = 0:
+	set(value):
+		amount = value
+		$Amount.text = str(value)
+		if amount <= 0:
+			item = null
+
+func set_amount(value: int):
+	amount = value
+
+func add_amount(value : int):
+	amount += value
+
+func _can_drop_data(_at_position, data):
+	if "item" in data:
+		return is_instance_of(data.item, Item)
+	return false
+
+func _drop_data(_at_position, data):
+	var temp = item
+	item = data.item
+	data.item = temp
+	
+	temp = amount
+	amount = data.amount
+	data.amount = temp
+
+
+func _get_drag_data(_at_position):
+	if item:
+		var preview_texture = TextureRect.new()
+		
+		preview_texture.texture = item.icon
+		preview_texture.size = Vector2(50, 50)
+		preview_texture.position = -Vector2(8, 8)
+		
+		var preview = Control.new()
+		preview.add_child(preview_texture)
+		set_drag_preview(preview)
+		
+	return self
