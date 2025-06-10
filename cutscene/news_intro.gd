0e2ef74a11f574cb726d8431df5b1f485402ed5c
Aisyah <aisyahahmadnizam@gmail.com>
2025-06-10 21:33:44 +0800

    cutscene

--- cutscene/news_intro.gd
+++ cutscene/news_intro.gd
@@ -2,6 +2,10 @@ extends Control
 signal finished
 
 func _ready():
-	# main animasi/video/news intro
-	await get_tree().create_timer(3.0).timeout  # adjust ikut panjang clip
+	
+	await get_tree().create_timer(4.0).timeout  
 	emit_signal("finished")
+
+	var next_scene = preload("res://cutscene/cutscene_1_news.tscn").instantiate()
+	get_tree().current_scene.get_parent().add_child(next_scene)
+	get_tree().current_scene.queue_free()
