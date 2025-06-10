ba4f409b1514b9cdf8c74bc2c1f3528e1d23c000
Luqman Hakim Bin Muhammad Fahmi <luqman5421er@gmail.com>
2025-06-10 18:39:10 +0800

    Revert "Inventory"
    This reverts commit 513a69dabeba977be9025f4172a89bbeb6b088b2.

--- GUI Scripts/inventory_slot.gd
+++ GUI Scripts/inventory_slot.gd
@@ -1,1 +1,24 @@
-extends Button
+extends TextureRect
+
+@export var itemResource : Item
+
+func set_new_data(resource: Item):
+	itemResource = resource
+	
+	if resource != null:
+		texture = itemResource.icon
+		itemResource.inventarSlot = get_parent().name
+		itemResource.InventarPosition = int(name.split("Slot")[1])
+	else:
+		itemResource = null
+		texture = null
+		
+func get_slot_name():
+	var parentName = get_parent().name
+	var slotNumber = name.split("Slot")[1]
+	
+	return parentName + slotNumber
+
+func delete_resource():
+	texture = null
+	itemResource = null
--- Inventory/Scriptss/slot_data.gd
+++
@@ -1,5 +0,0 @@
-class_name SlotData extends Resource
-
-
-@export var item_data : Item
-@export var quantity : int = 0
