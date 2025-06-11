47a6e98eeeee81ea9534a35e7a355063ef3eff92
Aisyah <aisyahahmadnizam@gmail.com>
2025-06-11 17:37:42 +0800

    cutscene

--- cutscene/news_intro.gd
+++ cutscene/news_intro.gd
@@ -4,8 +4,9 @@ signal finished
 func _ready():
 	
 	await get_tree().create_timer(4.0).timeout  
-	emit_signal("finished")
 
 	var next_scene = preload("res://cutscene/cutscene_1_news.tscn").instantiate()
 	get_tree().current_scene.get_parent().add_child(next_scene)
 	get_tree().current_scene.queue_free()
+
+	emit_signal("finished")
