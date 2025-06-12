0a45c3d990e6148880996272a66a26561039600d
Luqman Hakim Bin Muhammad Fahmi <luqman5421er@gmail.com>
2025-06-13 01:52:43 +0800

    Every level has its own music / Sound fixes / Main Menu music changed

--- GUI Scripts/credit_scene.gd
+++ GUI Scripts/credit_scene.gd
@@ -1,6 +1,7 @@
 extends Control
 
 func _ready():
+	Ui.hide()
 	$AnimationPlayer.play("Credit_Scene")
 	await $AnimationPlayer.animation_finished
 	Functions.load_screen_to_scene("res://GUI Scenes/StartMenu.tscn")
