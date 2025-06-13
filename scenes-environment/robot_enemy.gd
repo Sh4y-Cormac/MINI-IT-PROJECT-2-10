0c433ab0f7aaea7873ea2f079a63f51d70553ba1
Umar <umarzaidtenby@gmail.com>
2025-06-13 09:03:20 +0800

    forgot again, changing to brnach now

--- scenes-environment/robot_enemy.gd
+++ scenes-environment/robot_enemy.gd
@@ -11,9 +11,11 @@ var is_enemy_chasing: bool
 var dir: Vector2
 var player: CharacterBody2D
 
-var health = 80
-var health_max = 0
+var health = 200
+var health_max = 200
 var health_min = 0 
+const gravity = 900 
+
 var dead: bool = false
 var taking_damage: bool = false
 var is_roaming: bool
@@ -27,23 +29,23 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	Global.robotDamageAmount = damage_to_deal
 	Global.robotDamageZone = robot_deal_damage_area
+	player = Global.playerBody
 	
 	if Global.playerAlive:
 		is_enemy_chasing = true
 	if !Global.playerAlive:
 		is_enemy_chasing = false
 	
-	if is_on_floor() and dead:
-		await get_tree().create_timer(3.0).timeout
-		Global.playerGold += droppedGold
-		self.queue_free()
+	if !is_on_floor():
+		velocity.y += gravity * delta
+		velocity.x = 0
 		
 	move(delta)
 	animation()
 	move_and_slide()
 
 func move(delta):
-	player = Global.playerBody
+	
 	if !dead:
 		if !is_enemy_chasing:
 			velocity += dir * speed * delta
@@ -88,11 +90,7 @@ func animation():
 	elif dead and is_roaming:
 		is_roaming = false
 		animated_sprite.play("death")
-		set_collision_layer_value(1, true)
-		set_collision_layer_value(2, false)
-		set_collision_mask_value(1, true)
-		set_collision_mask_value(2, false)
-
+	
 func _on_timer_timeout() -> void:
 	$Timer.wait_time = choose([1.0, 1.5, 2.0])
 	if !is_enemy_chasing:
@@ -114,5 +112,5 @@ func take_damage(damage):
 	taking_damage = true
 	if health <0:
 		health = 0
-		dead = true	
-	print(str(self), "current health is ", health)
+		dead = true
+	
