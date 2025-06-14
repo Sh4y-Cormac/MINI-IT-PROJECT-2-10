be710f2dc8dbde7d13248128c4282c806cc9f894
Aisyah <aisyahahmadnizam@gmail.com>
2025-06-14 18:07:51 +0800

    fixed cutscene intro and added skip button

--- cutscene/cutscene_2_vid.gd
+++ cutscene/cutscene_2_vid.gd
@@ -1,8 +1,23 @@
 extends Control
 
-@onready var video_player = $VideoStreamPlayer
+@onready var video_player := $VideoStreamPlayer
+var can_skip := true
 
-func _ready():
+func _ready() -> void:
 	video_player.play()
-	await video_player.finished
-	Functions.load_screen_to_scene(Global.next_scene_after_cutscene)
+	video_player.finished.connect(_on_video_finished)
+
+func _process(delta: float) -> void:
+	if can_skip and Input.is_action_just_pressed("ui_accept"):
+		_end_cutscene()
+
+func _on_video_finished() -> void:
+	_end_cutscene()
+
+func _end_cutscene() -> void:
+	if not can_skip:
+		return
+	can_skip = false
+	if video_player.is_playing():
+		video_player.stop()
+	get_tree().change_scene_to_file(Global.next_scene_after_cutscene)
