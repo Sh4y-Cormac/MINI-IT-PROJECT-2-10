9d0f55e6064195dc413bc833cc25fba46a2192c7
Umar <umarzaidtenby@gmail.com>
2025-06-14 16:14:08 +0800

    fixed the bug and added health chests

--- scenes-player-movement/player_health_bar.gd
+++ scenes-player-movement/player_health_bar.gd
@@ -1,9 +1,10 @@
 extends ProgressBar
 
 func _ready():
-	self.max_value = 100
+	self.max_value = Global.playerMaxHealth
 	self.visible = true      
 	self.value = Global.playerHealth
 	
 func _process(delta):
+	self.max_value = Global.playerMaxHealth
 	self.value = Global.playerHealth
